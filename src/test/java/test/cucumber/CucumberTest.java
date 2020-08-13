8702307fcd58aa25216aa1af6598426ea07190dd
Padmy\dexterfloy <andru.cornea@google.com>
2020-08-13 18:28:56 +0300

    theTest Adding basic framework elements

---
+++ src/test/java/test/cucumber/CucumberTest.java
@@ -0,0 +1,12 @@
+package test.cucumber;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(junit = "--step-notifications", tags = "not @ignore")
+public class CucumberTest {
+
+}
